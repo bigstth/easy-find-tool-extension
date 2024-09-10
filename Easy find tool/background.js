66451823221659f4f527a67f1f6596d39db58f79
bigstth~ <sitthidsn@gmail.com>
2024-09-10 23:59:40 +0700

    remove unuse folder

--- Easy find tool/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log('Extension installed and command registered.')
-})
-
-chrome.commands.onCommand.addListener((command) => {
-    if (command === 'open_extension') {
-        chrome.action.openPopup()
-    }
-})
